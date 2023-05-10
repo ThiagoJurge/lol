e1b21c735bfe40f75e0c1be87deb8fc265ec7015
ThiagoJurge <67167598+ThiagoJurge@users.noreply.github.com>
2023-05-10 18:58:21 -0300

    - Title format changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { Card } from "antd";
 import MainTable from "./components/MainTable/MainTable";
+import Title from "antd/es/typography/Title";
 
 function App() {
 
   return (
-    <Card title="#Cade Faol ?">
+    <Card title={<center><Title level={1}>Cade Faol ?</Title></center>}>
       <MainTable />
     </Card>
   );
