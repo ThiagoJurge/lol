c23da2df779146271d2ee5a80ec8379c99f2a39b
ThiagoJurge <67167598+ThiagoJurge@users.noreply.github.com>
2023-05-10 19:41:01 -0300

    - Added Icons

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@ import React from "react";
 import { Card } from "antd";
 import MainTable from "./components/MainTable/MainTable";
 import Title from "antd/es/typography/Title";
+import { ImLocation } from "react-icons/im"
 
 function App() {
 
   return (
-    <Card title={<center><Title level={1}>Cade Faol ?</Title></center>}>
+    <Card title={<center><Title level={1}><ImLocation /> Cade Faol </Title></center>}>
       <MainTable />
     </Card>
   );
--- src/components/ModalTable/ModalTable.jsx
+++ src/components/ModalTable/ModalTable.jsx
@@ -2,6 +2,8 @@ import { Table } from 'antd'
 import React from 'react'
 import { getDate } from '../../utils/getDate';
 import { formatCoord } from '../../utils/formatCoord';
+import { CiLocationOff } from 'react-icons/ci'
+import Title from 'antd/es/typography/Title';
 
 const ModalTable = ({ data }) => {
     const columns = [
@@ -50,11 +52,17 @@ const ModalTable = ({ data }) => {
         },
     ];
     return (
+        <>
+        {data[0] ? 
+        
         <Table
             columns={columns}
             dataSource={data}
             tableLayout="auto"
-        />
+        /> : 
+<center><Title level={1} ><CiLocationOff color='red'/> Sem comunicação com o GPS</Title></center>        
+        }
+        </>
     )
 }
 
